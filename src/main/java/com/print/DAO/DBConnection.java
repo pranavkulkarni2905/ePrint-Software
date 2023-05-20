75435e85d46e751ee176efcb2179cdd8a2d3b807
Pranav Kulkarni <pranavkulkarni1110@gmail.com>
2023-05-20 11:16:06 +0530

    created db connection

--- src/main/java/com/print/DAO/DBConnection.java
+++ src/main/java/com/print/DAO/DBConnection.java
@@ -1,5 +1,31 @@
 package com.print.DAO;
 
-public class DBConnection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+public class DBConnection {
+	public static void main(String args[]){  
+
+	//public static Connection getConnection() {
+		Connection con = null;
+		try {
+			// load drivers
+			Class.forName("com.mysql.cj.jdbc.Driver");  
+			// connection to data Base
+
+			con=DriverManager.getConnection(  
+					"jdbc:mysql://localhost:3306/eprint","root","root");  
+			System.out.println("connection...");
+
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//return con;
+	//}
+	}
 }
