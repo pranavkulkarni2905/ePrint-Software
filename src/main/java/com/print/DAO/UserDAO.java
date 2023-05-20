d06f7b4b1cabe0394bb9577c832b26efb571c540
saurabhasnare <66157342+saurabhasnare@users.noreply.github.com>
2023-05-20 18:44:22 +0530

    user login feature added

--- src/main/java/com/print/DAO/UserDAO.java
+++ src/main/java/com/print/DAO/UserDAO.java
@@ -2,6 +2,7 @@ package com.print.DAO;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.Date;
@@ -9,11 +10,11 @@ import java.util.Date;
 import com.print.model.User;
 
 public class UserDAO {
-	Connection con=null;
+	Connection con = null;
 	PreparedStatement ps;
-	
+
 	public int registerUser(User u) {
-		
+
 		int i = 0;
 		Timestamp date = new Timestamp(new Date().getTime());
 		con = DBConnection.getConnection();
@@ -25,7 +26,7 @@ public class UserDAO {
 			ps.setString(3, u.getName());
 			ps.setString(4, u.getPhone());
 			ps.setString(5, u.getDegree());
-			ps.setString(6,u.getBranch());
+			ps.setString(6, u.getBranch());
 			ps.setTimestamp(7, date);
 			ps.setString(8, "No");
 			i = ps.executeUpdate();
@@ -35,39 +36,46 @@ public class UserDAO {
 		}
 		return i;
 	}
-	public String sendotp(String name,String email) {
-        com.print.email.SendMail sm=new com.print.email.SendMail();
-		
-		String otp=sm.generateOTP();
-		
+
+	public String sendotp(String name, String email) {
+		com.print.email.SendMail sm = new com.print.email.SendMail();
+
+		String otp = sm.generateOTP();
+
 		final String subject = "Verify Your Account | ePrint Software";
-		final String messg = "Dear "+name+",\n\n"
-				+"Thank you for choosing ePrint Software! To ensure the security of your account, we require a verification process to activate your ePrint account. Please follow the instructions below to verify your account and complete the registration process.\r\n"
-				+ "\r\n"
-				+ "Step 1: Retrieve the OTP (One-Time Password)\r\n"
+		final String messg = "Dear " + name + ",\n\n"
+				+ "Thank you for choosing ePrint Software! To ensure the security of your account, we require a verification process to activate your ePrint account. Please follow the instructions below to verify your account and complete the registration process.\r\n"
+				+ "\r\n" + "Step 1: Retrieve the OTP (One-Time Password)\r\n"
 				+ "This email contais a six-digit OTP (One-Time Password). Please keep this email secure and do not share the OTP with anyone.\r\n"
-				+ "\r\n"
-				+ "Step 2: Account Verification \nEnter the six-digit OTP you received in the email.\r\n"
+				+ "\r\n" + "Step 2: Account Verification \nEnter the six-digit OTP you received in the email.\r\n"
 				+ "Click on the \"Verify\" or \"Submit\" button to complete the verification process.\r\n"
 				+ "Please note that the OTP is valid for a limited time. If you do not enter the OTP within [time frame], you will need to request a new OTP.\r\n"
-				+ "\r\n"
-				+ "OTP- "+otp+"\n\nIf you did not request this verification, please disregard this email. However, we highly recommend contacting our support team immediately for further assistance.\r\n"
+				+ "\r\n" + "OTP- " + otp
+				+ "\n\nIf you did not request this verification, please disregard this email. However, we highly recommend contacting our support team immediately for further assistance.\r\n"
 				+ "\r\n"
 				+ "If you have any questions or encounter any issues during the verification process, please don't hesitate to reach out to our customer support team at [support email/phone number].\r\n"
 				+ "\r\n"
 				+ "Thank you for choosing ePrint Software. We look forward to providing you with a seamless and secure printing experience!\r\n"
-				+ "\r\n"
-				+ "Best regards,\r\n"
-				+ "\r\n"
-				+ "ePrint Software Team\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "\r\n"
-				+ "";
+				+ "\r\n" + "Best regards,\r\n" + "\r\n" + "ePrint Software Team\r\n" + "\r\n" + "\r\n" + "\r\n" + "\r\n"
+				+ "\r\n" + "\r\n" + "";
 		sm.sendEmail(email, messg, subject);
 		return otp;
 	}
+
+	public ResultSet loginUser(String email, String password) {
+
+		con = DBConnection.getConnection();
+		ResultSet rs = null;
+		try {
+			ps = con.prepareStatement("select * from user where email=? and password=?");
+			ps.setString(1, email);
+			ps.setString(2, password);
+			rs = ps.executeQuery();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return rs;
+	}
+
 }
